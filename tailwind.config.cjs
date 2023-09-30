b20e65619b6407a6102a8056d74f78ea6c77ef9e
Divyanshu Shekhar <imdshekhar@gmail.com>
2023-09-30 16:37:19 +0530

    feat: top navbar created

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'primary': {
+          "blue":'#194DAB',
+          'blue-light': '#6880AB',
+        },
+        'background': {
+          'light': '#0C0C14',
+          'dark': '#0B0C14',
+        },
+        'accent': '#939DB8',
+      },
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif'],
+      },
+    }
   },
   plugins: []
 }

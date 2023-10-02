0474a0eee279c062e39396b8bbc033876c39aed5
Ankit <ankitkumar19041@gmail.com>
2023-10-02 22:51:37 +0530

    student page almost done

---
+++ src/constants/students/information.ts
@@ -0,0 +1,50 @@
+interface Company {
+  path: string
+}
+
+const CompaniesInfo: Company[] = [
+  {
+    path: '/redis.svg'
+  },
+  {
+    path: '/redis.svg'
+  },
+  {
+    path: '/redis.svg'
+  },
+  {
+    path: '/redis.svg'
+  },
+  {
+    path: '/redis.svg'
+  },
+  {
+    path: '/redis.svg'
+  },
+  {
+    path: '/redis.svg'
+  },
+  {
+    path: '/redis.svg'
+  },
+  {
+    path: '/redis.svg'
+  },
+  {
+    path: '/redis.svg'
+  },
+  {
+    path: '/redis.svg'
+  },
+  {
+    path: '/redis.svg'
+  },
+  {
+    path: '/redis.svg'
+  },
+  {
+    path: '/redis.svg'
+  }
+]
+
+export { CompaniesInfo as default, type Company }

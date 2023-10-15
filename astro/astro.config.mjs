c2fd8ad6f683e485022a59da2b8b95eab4f602e5
Divyanshu Shekhar <imdshekhar@gmail.com>
2023-10-15 12:47:59 +0530

    feat: SSR using node adapter

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config'
-
+import node from '@astrojs/node'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: 'server',
+  server: { port: 5002, host: '0.0.0.0' },
+  adapter: node({
+    mode: 'standalone'
+  }),
+  integrations: [tailwind()],
+  site: 'https://tyup.org'
 })

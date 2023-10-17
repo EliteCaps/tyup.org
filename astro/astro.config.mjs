9e91140b3cdad0b71ad90bfd3f3e020e8f0e3f05
Divyanshu Shekhar <imdshekhar@gmail.com>
2023-10-17 11:15:01 +0530

    feat: campus & recruit contact page

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,13 +2,20 @@ import { defineConfig } from 'astro/config'
 import node from '@astrojs/node'
 import tailwind from '@astrojs/tailwind'
 
+const isProd = import.meta.env.PROD
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  server: { port: 5002, host: '0.0.0.0' },
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: isProd ? 'hybrid' : 'static',
+  server: isProd ? { port: 5002, host: '0.0.0.0' } : undefined,
+  build: {
+    trailingSlash: 'ignore'
+  },
+  adapter: isProd
+    ? node({
+        mode: 'standalone'
+      })
+    : undefined,
   integrations: [tailwind()],
   site: 'https://tyup.org'
 })
